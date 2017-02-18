972ef185a756c306db71fb1f8d5ebb5b884103fb
mslos <ms7576@nyu.edu>
2017-02-18 20:50:07 +0400

    added server side from tutorial

--- FTPserver.c
+++ FTPserver.c
@@ -7,19 +7,31 @@
 #include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
 
-
+#define BUFFER_SIZE 500
 
 int main (int argc, char ** argv) {
 	for (int i = 0; i<argc; i++) {
 		printf("Argument %d is %s\n", i, argv[i]);
 	}
 	 //tcp protocol,, same as opening a file
-	int port, sock;
+	int port, sock, len, client_fd;
 	char * ip_addr;
-	struct sockaddr_in server_addr;
+	struct sockaddr_in server_addr, client_addr;
+
+	char buffer[BUFFER_SIZE];
+
 	open_socket(&server_addr, port, ip_addr, &sock);
 
-	
+	bind(sock, &server_addr, sizeof(server_addr));
+	listen(sock,5);
+	len = sizeof(client_addr);
+	while(1) {
+		client_fd = accept(sock,(const struct sockaddr *) &client_addr,&len);
+		//will block if there are 0 clients
+
+		int size = read(client_fd, buffer, sizeof(buffer));
+		printf("%s",buffer);
+	}
 }
 
 int open_socket(struct sockaddr_in * myaddr, int port, char * addr, int * sock) {
