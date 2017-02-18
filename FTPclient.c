88d1aa1ca4d777878516f89f65d34ab611e1e8c6
mslos <ms7576@nyu.edu>
2017-02-19 01:02:54 +0400

    Tried to debug the server code from lecture, using input from cmd line. ISSUE: Connect() in client does not work

--- FTPclient.c
+++ FTPclient.c
@@ -6,9 +6,54 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
+#include <string.h>
+
+#define BUFFER_SIZE 500
+
+int open_socket(struct sockaddr_in * myaddr, int * port, char * addr, int * sock);
 
 int main (int argc, char ** argv) {
 	for (int i = 0; i<argc; i++) {
 		printf("Argument %d is %s\n", i, argv[i]);
 	}
+	
+	int port, sock_fd, len, client_fd;
+	char * ip_addr;
+	struct sockaddr_in server_addr;
+	char buffer[BUFFER_SIZE];
+	bcopy(&argv[3], buffer, sizeof(argv[3]));
+
+
+	//read in port number and address from args
+	ip_addr = argv[1];
+	port = atoi(argv[2]);
+
+	open_socket(&server_addr, &port, ip_addr, &sock_fd);
+
+	int ret = connect(sock_fd,(const struct sockaddr *) &server_addr, sizeof(server_addr));
+
+	if (ret < 0) {
+		printf("Error happened while connecting in client\n");
+		return 1;
+	}
+	
+	write(sock_fd, buffer, strlen(buffer)+1);
+	close(sock_fd);
+}
+
+int open_socket(struct sockaddr_in * myaddr, int * port, char * addr, int * sock) {
+	*sock = socket(AF_INET, SOCK_STREAM,0);
+
+	if (sock < 0) {
+      printf(stderr,"Error opening socket");
+   }
+
+	myaddr->sin_family = AF_INET;
+	myaddr->sin_port = htons(*port);
+	
+	if( inet_aton(addr, &(myaddr->sin_addr.s_addr))==0 ) {
+		fprintf(stderr, "Error, cannot translate IP into binary");
+	}
+
+	return 0;
 }
--- FTPserver.c
+++ FTPserver.c
@@ -6,8 +6,12 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
+#include <string.h>
 
 #define BUFFER_SIZE 500
+#define MAX_NUM_OF_CLIENTS 5
+
+int open_socket(struct sockaddr_in * myaddr, int *port, char * addr, int * sock);
 
 int main (int argc, char ** argv) {
 	for (int i = 0; i<argc; i++) {
@@ -17,14 +21,23 @@ int main (int argc, char ** argv) {
 	int port, sock, len, client_fd;
 	char * ip_addr;
 	struct sockaddr_in server_addr, client_addr;
-
 	char buffer[BUFFER_SIZE];
+	len = sizeof(client_addr); //necessary since accept requires lvalue
 
-	open_socket(&server_addr, port, ip_addr, &sock);
+	//read in port number and address from args
+	ip_addr = argv[1];
+	port = atoi(argv[2]);
 
-	bind(sock, &server_addr, sizeof(server_addr));
-	listen(sock,5);
-	len = sizeof(client_addr);
+	//create socket descriptor
+	open_socket(&server_addr, &port, ip_addr, &sock);
+	//bind socket to port and address
+	if (bind(sock, &server_addr, sizeof(server_addr)) < 0) {
+      printf(stderr,"Could not bind socket");
+   	}
+	//listen for clients, max number specified by MAX_NUM_OF_CLIENTS, block until first connection
+	listen(sock,MAX_NUM_OF_CLIENTS);
+
+
 	while(1) {
 		client_fd = accept(sock,(const struct sockaddr *) &client_addr,&len);
 		//will block if there are 0 clients
@@ -34,11 +47,11 @@ int main (int argc, char ** argv) {
 	}
 }
 
-int open_socket(struct sockaddr_in * myaddr, int port, char * addr, int * sock) {
+int open_socket(struct sockaddr_in * myaddr, int *port, char * addr, int * sock) {
 	*sock = socket(AF_INET, SOCK_STREAM,0);
 	myaddr->sin_family = AF_INET;
 	myaddr->sin_port = htons(port);
 	
-	if( inet_aton(addr, &(myaddr->sin_addr.s_addr))==0 )
+	if( inet_aton(addr,&(myaddr->sin_addr.s_addr))  == 0 )
 		fprintf(stderr, "Error, cannot translate IP into binary");
 }
